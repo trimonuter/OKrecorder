707892e27b75d320ef9d11091ea1f338a6aae778
trimonuter <mrqaist@gmail.com>
2023-08-02 08:22:09 +0700

    added shortcut keys for topic name popup

--- script.js
+++ script.js
@@ -8,24 +8,47 @@ function cloneTopic(title) {
     return clone;
 }
 
+// Add topic button pressed
 const popup = document.getElementById("topicNameContainer");
+const inputTopicBox = document.getElementById('inputTopicName');
 addTopic.addEventListener('click', () => {
     popup.style.display = "flex";
+    inputTopicBox.focus();
+
 })
 
+// Cancel popup functions
 const cancelPopup = document.getElementById("cancel-popup");
 cancelPopup.addEventListener('click', () => {
-    popup.style.display = "none"
+    popup.style.display = "none";
 })
 
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 27) {
+        popup.style.display = "none";
+    }
+})
+
+// Submit topic name
 const button = document.getElementById("submitTopicName");
 button.addEventListener("click", function(){
+    appendTopic();
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+        appendTopic();
+    }
+})
+
+// Helper functions
+function appendTopic(){
     const inputName = document.getElementById("inputTopicName").value;
     topicsSection.append(cloneTopic(inputName));
     
     popup.style.display = "none";
     refreshCards();
-})
+}
 
 function refreshCards() {
     const cardList = document.querySelectorAll(".topic-card")
